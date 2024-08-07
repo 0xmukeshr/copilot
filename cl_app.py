c233eeb94f5bbadbf512549eac54dade94b259b4
Mukesh <167955129+0xmukeshr@users.noreply.github.com>
2024-08-07 17:59:06 +0530

    first commit

---
+++ cl_app.py
@@ -0,0 +1,48 @@
+
+from openai import AsyncOpenAI
+import chainlit as cl
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = AsyncOpenAI(api_key="sk-proj-WP5LkGDQ7Ccqq8kE3CMFT3BlbkFJn8EiWUdSwnZn9sRyQux7")
+
+
+settings = {
+    "model": "gpt-3.5-turbo",
+    "temperature": 0.7,
+    "max_tokens": 500,
+    "top_p": 1,
+    "frequency_penalty": 0,
+    "presence_penalty": 0,
+}
+
+
+@cl.on_chat_start
+def start_chat():
+    cl.user_session.set(
+        "message_history",
+        [{"role": "system", "content": "You are a helpful assistant."}],
+    )
+
+
+@cl.on_message
+async def main(message: cl.Message):
+    print("Message")
+    message_history = cl.user_session.get("message_history")
+    message_history.append({"role": "user", "content": message.content})
+
+    msg = cl.Message(content="")
+    await msg.send()
+
+    stream = await client.chat.completions.create(
+        messages=message_history, stream=True, **settings
+    )
+
+    async for part in stream:
+        if token := part.choices[0].delta.content or "":
+            await msg.stream_token(token)
+
+    message_history.append({"role": "assistant", "content": msg.content})
+    await msg.update()
